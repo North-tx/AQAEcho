a54436103ed40bc13ae8c9d36dcf87f2b066a52b
North-tx <120264083+North-tx@users.noreply.github.com>
2023-04-23 14:53:19 +0300

    initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Data"))
+                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
+    }
+}
